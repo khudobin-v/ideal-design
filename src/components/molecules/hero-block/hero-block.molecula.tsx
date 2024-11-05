41ef198746a49ecb1a50cb1807579a67d5a0c1e3
Василий Худобин (MacOS) <khudobin_v@icloud.com>
2024-11-05 09:43:37 +0300

    Обновление Hero

--- src/components/molecules/hero-block/hero-block.molecula.tsx
+++ src/components/molecules/hero-block/hero-block.molecula.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-scroll";
 export const HeroBlock = () => {
   return (
     <div className="flex h-screen w-1/2 flex-col items-center justify-center gap-4 selection:bg-white selection:text-black max-lg:w-full max-lg:px-12">
-      <h1 className="text-6xl font-bold tracking-wide max-lg:text-5xl">
+      <h1 className="text-6xl font-bold uppercase tracking-wide max-lg:text-5xl">
         Ideal
       </h1>
       <p className="text-center text-base max-lg:text-sm">
