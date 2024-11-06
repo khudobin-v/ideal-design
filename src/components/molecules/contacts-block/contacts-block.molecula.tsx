617294cc25065b398390e4ca0d255672d00d11f5
Василий Худобин (MacOS) <khudobin_v@icloud.com>
2024-11-06 22:00:20 +0300

    Добавление новых ссылок и исправление отображения на мобильных устройствах

--- src/components/molecules/contacts-block/contacts-block.molecula.tsx
+++ src/components/molecules/contacts-block/contacts-block.molecula.tsx
@@ -1,14 +1,17 @@
 import { FaMapPin, FaUser } from "react-icons/fa";
+import { FaPhone, FaVk } from "react-icons/fa6";
 
 import Link from "next/link";
-import { FaPhone } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 
 export const ContactsBlock = () => {
   return (
     <div className="flex h-full w-full flex-col justify-around">
-      <h1 className="ml-20 text-5xl font-semibold uppercase">Контакты</h1>
-      <div className="ml-20">
+      <h1 className="ml-20 text-5xl font-semibold uppercase max-sm:ml-4">
+        Контакты
+      </h1>
+      <div className="ml-20 max-sm:ml-4">
         <ul className="flex flex-col gap-2 text-xl">
           <li className="flex items-center gap-2">
             <FaUser size={20} />
@@ -41,6 +44,24 @@ export const ContactsBlock = () => {
               href="mailto:ideal.studiadizaina@yandex.ru"
             >
               ideal.studiadizaina@yandex.ru
+            </Link>
+          </li>
+          <li className="flex items-center gap-2">
+            <FaVk size={20} />
+            <Link
+              className="hover:underline"
+              href="https://vk.com/idealdesign2024"
+            >
+              Мы ВКонтакте
+            </Link>
+          </li>
+          <li className="flex items-center gap-2">
+            <FaInstagram size={20} />
+            <Link
+              className="hover:underline"
+              href="https://www.instagram.com/ideal__interior30/profilecard/?igsh=eWF4OHlrcTZkamo1"
+            >
+              Наш Instagram
             </Link>
           </li>
         </ul>
