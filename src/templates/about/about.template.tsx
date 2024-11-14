7daf3643d323554c1e924348413ef3a21c95dc59
Василий Худобин (MacOS) <khudobin_v@icloud.com>
2024-11-14 12:27:09 +0300

    Добавление адаптивности

--- src/templates/about/about.template.tsx
+++ src/templates/about/about.template.tsx
@@ -2,7 +2,7 @@ import { AboutBlock } from "@/components";
 
 export const About = () => {
   return (
-    <div id="about" className="h-screen bg-white text-black">
+    <div id="about" className="min-h-screen bg-white text-black">
       <AboutBlock />
     </div>
   );
